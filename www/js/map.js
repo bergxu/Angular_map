30a6e01769574994f45a779c6494c523f8a68ce8
bergxu <pengfei.xu@itbconsult.com>
2014-04-17 18:06:43 +0800

    update top bottom css and logic

--- www/js/map.js
+++ www/js/map.js
@@ -14,6 +14,7 @@ app.controller("appCtrl",function($scope, $http){
 	
 	$scope.mapList = [];
 	
+	
 	$scope.gotoCurrentLocation= function(){
 		navigator.geolocation.getCurrentPosition(function (position) {
 	        var c = position.coords;
@@ -29,56 +30,30 @@ app.controller("appCtrl",function($scope, $http){
 	$scope.gotoCurrentLocation();
 	 
 	$scope.gotoLocation = function (lat, lon) {
-		//$scope.clearOverlays();
         if ($scope.lat != lat || $scope.lon != lon) {
             $scope.loc = {
             		latitude: lat, 
         	        longitude: lon
     	    	};
 	    	$scope.zoom =14;
-	    	
-//	    	angular.forEach($scope.myMarkers, function(marker){
-//	    		google.maps.event.addListener(
-//			      marker,
-//			      'click',
-//			      (function( marker , scope, localLatLng ){
-//			        return function(){
-//			         // var content = '<div id="infowindow_content" >hello world</div>';
-//			          //scope.latLng = localLatLng;
-//			         // var compiled = $compile(content)(scope);
-//			          //scope.$apply();
-//			          //infowindow.setContent( compiled[0].innerHTML );
-//			          //infowindow.open( Map , marker );
-//			        };//return fn()
-//			      })( marker , scope, $scope.myMarkers[i].locations )
-//	    	});
-	    	
-	    	
-	    	
             $scope.$apply();
         }
     };
 	
 	$scope.search = "";
 	$scope.geoCode = function(){
-		$scope.distance = parseInt($("#selectedDistance").val());
 		if($scope.distance > 0){
 	    	var cb = function(markersArray){
 	    		$scope.myMarkers = markersArray;
 	    		var slCb = function(searchedLatlng){
 	    			$scope.myMarkers.push(searchedLatlng);
 	    			$scope.gotoLocation($scope.loc.latitude, $scope.loc.longitude);
-	    			
-//	    			angular.forEach($scope.myMarkers,function(marker){
-//	    				var myLatLng = new google.maps.LatLng(marker.latitude, marker.longitude);
-//	    			});
 	    		}; 
 	    		$scope.searchLocation(slCb);
 	    	};
 	    	$scope.getData(cb);
 	    }else if($scope.distance == 0){
 	    	var slCb = function(searchedLatlng){
-    			//$scope.myMarkers.push(searchedLatlng);
 	    		$scope.myMarkers=[];
 	    		var latlng={
     				latitude: searchedLatlng.latitude, 
@@ -91,12 +66,6 @@ app.controller("appCtrl",function($scope, $http){
 	    }
 	};
 	
-	$scope.myTest = function(){
-		alert(111);
-		alert($scope.control.getGMap());	
-	}
-	
-	
 	$scope.searchLocation = function(callback){
 		if ($scope.search && $scope.search.length > 0) {
 	        if (!this.geocoder) this.geocoder = new google.maps.Geocoder();
@@ -107,7 +76,6 @@ app.controller("appCtrl",function($scope, $http){
 	            		latitude: loc.lat(),
 	        	        longitude: loc.lng()
 	                }
-	               // $scope.gotoLocation(loc.lat(), loc.lng());
 	                callback($scope.loc);
 	            } else {
 	                alert("Sorry, this search produced no results.");
@@ -125,8 +93,6 @@ app.controller("appCtrl",function($scope, $http){
 		$scope.myMarkers = [];
 		var myData = {
 		        "accountId" : "001L000000PKm8x",
-//		        "lat": $scope.loc.latitude,
-//		        "lng": $scope.loc.longitude,
 		        "lat": "31.110447",
 		        "lng": "121.374097",
 		        "distanceUnit": "km",
@@ -159,9 +125,20 @@ app.controller("appCtrl",function($scope, $http){
 		});
 	};
 	
-	
-	 
-	 
+	$scope.mapClick = function(){
+		$("#clickMap").addClass("bottomBtnClick").removeClass("bottomBtnUnclick");
+		$("#clickList").addClass("bottomBtnUnclick").removeClass("bottomBtnClick");
+	};
+	$scope.listClick = function(){
+		$("#clickMap").removeClass("bottomBtnClick").addClass("bottomBtnUnclick");
+		$("#clickList").removeClass("bottomBtnUnclick").addClass("bottomBtnClick");
+	};
+	$scope.distance = 0;
+	$scope.metadata = {
+			picklists : {
+				distance : [0,5,10,15,20,25,30,35,40,45,50]
+			} 
+	}
 	 
 	 
 	 
